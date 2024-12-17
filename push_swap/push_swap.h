51592656e443c2d8ff856869e9d6b8f66a7535d7
ilorez <zobirnajdaoui@gmail.com>
2024-12-17 18:30:31 +0100

    some update in push swap

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 17:35:08 by znajdaou          #+#    #+#             */
-/*   Updated: 2024/12/17 17:39:28 by znajdaou         ###   ########.fr       */
+/*   Updated: 2024/12/17 18:14:53 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@
 #define INT_MAX 2147483647  
 #define INT_MIN -2147483648
 
-
+bool ft_create_stack(int ac, char **av, t_list *stack_a);
+bool ft_is_all_digits(char *str);
+bool ft_free_list_of_strs(char **strs);
 
 #endif
 
