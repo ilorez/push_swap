7d2dd8e21d78a54c8c4a9785fb012702952ac1f3
ilorez <zobirnajdaoui@gmail.com>
2024-12-19 09:39:43 +0100

    create ft_print_stack for debugging requists and some norm update in push swap

--- push_swap/ft_debugging.c
+++ push_swap/ft_debugging.c
@@ -1,23 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   ft_debugging.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/17 17:35:08 by znajdaou          #+#    #+#             */
-/*   Updated: 2024/12/19 08:47:53 by znajdaou         ###   ########.fr       */
+/*   Created: 2024/12/19 09:27:57 by znajdaou          #+#    #+#             */
+/*   Updated: 2024/12/19 09:32:30 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#include "../libft/ft_printf/ft_printf.h"
+#include "../libft/libft.h"
 
-# define INT_MAX 2147483647
-# define INT_MIN -2147483648
+int	ft_print_stack(t_list stack)
+{
+	int	cound;
 
-t_bool	ft_create_stack(int ac, char **av, t_list *stack_a);
-t_bool	ft_is_all_digits(char *str);
-t_bool	ft_free_list_of_strs(char **strs);
-
-#endif
+	count = ft_printf("==================");
+	while (*stack)
+		count += ft_printf("|%d|", (int)stack->content);
+	count += ft_printf("==================");
+	return (count);
+}
--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -1,55 +1,75 @@
-#include "push_swap.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/19 08:51:01 by znajdaou          #+#    #+#             */
+/*   Updated: 2024/12/19 09:35:47 by znajdaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/t_bool.h"
 #include "../libft/libft.h"
-#include "../includes/bool.h"
+#include "push_swap.h"
 
-bool ft_is_all_digits(char *str)
+t_bool	ft_is_all_digits(char *str)
 {
-  while (*str)
-    if (!ft_isdigit(*str))
-      return (false);
-  return (true);
+	while (*str)
+		if (!ft_isdigit(*str))
+			return (false);
+	return (true);
 }
 
-bool ft_create_stack(int ac, char **av, t_list *stack_a)
+t_bool	ft_create_stack(int ac, char **av, t_list *stack_a)
 {
-  long long num;
-  char **str_nums;
-  char *tmp;
+	long long	num;
+	char		**str_nums;
+	char		*tmp;
+	t_list		*new_num;
 
-  while (ac--)
-  {
-    str_nums = ft_split(av[ac]);
-    if (!str_nums)
-      return (false);
-    while (*str_nums)
-    {
-      if (!ft_is_all_digits(*str_nums))
-        return (ft_free_str_lst(str_nums));
-      num = ft_atol(*str_nums);
-      if (num > INT_MAX || num < INT_MIN)
-      {
-        ft_printf("Error\nnot valid number\nfound a number out of INT range\n");
-        return (ft_free_str_lst(str_nums));
-      }
-      ft_lst_add_front((int)num, stack_a);
-      tmp = *str_nums;
-      ++str_nums;
-      free(tmp);
-    }
-    ft_free_str_lst(str_nums);
-  }
-  return (true);
+	while (ac--)
+	{
+		str_nums = ft_split(av[ac]);
+		if (!str_nums)
+			return (false);
+		while (*str_nums)
+		{
+			if (!ft_is_all_digits(*str_nums))
+				return (ft_free_str_lst(str_nums));
+			num = ft_atol(*str_nums);
+			if (num > INT_MAX || num < INT_MIN)
+			{
+				ft_printf("Error\nnot valid number\n \
+          found a number out of INT range\n");
+				return (ft_free_str_lst(str_nums));
+			}
+			new_num = ft_lstnew(&((int)num));
+			if (!new_num)
+			{
+				ft_printf("Error\nnew_num desn't allocated correctly!");
+				return (ft_free_str_lst(str_nums));
+			}
+			ft_lstadd_front(stack_a, new_num);
+			tmp = *str_nums;
+			++str_nums;
+			free(tmp);
+		}
+		ft_free_str_lst(str_nums);
+	}
+	return (true);
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-  t_list stack_a;
+	t_list	stack_a;
 
-  stack_a = NULL;
-  if (ac == 1)
-    ft_printf("Error\nNo numbers passed!:[\nto pass argument use for example %s 1 2 3 4 5", av[0]);
-  if (!ft_create_stack(--ac, &av[1], &stack_a))
-    ft_lst_clear(stack_a);
-  ft_print_stack(stack_a);
-
+	stack_a = NULL;
+	if (ac == 1)
+		ft_printf("Error\nNo numbers passed!:[\nto pass argument use for example \
+%s 1 2 3 4 5", av[0]);
+	if (!ft_create_stack(--ac, &av[1], &stack_a))
+		ft_lstclear(stack_a, free);
+	ft_print_stack(stack_a);
 }
